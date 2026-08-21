18d50a60d4c53aca3196d3adba28bb26150e260c
= <=>
2026-08-21 02:02:46 +0200

    save

--- src/intelligent_day_trading/rule_engine/core/signal_summary_builder.py
+++ src/intelligent_day_trading/rule_engine/core/signal_summary_builder.py
@@ -58,7 +58,7 @@ class SignalSummaryBuilder:
             summary[
                 side
             ][
-                signal["signal"]
+                signal["signal"].lower()
             ] += signal[
                 "confidence_percentage"
             ]
