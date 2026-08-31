e4a95abee32d7244b5a81c20e4fb364b3b136eef
= <=>
2026-08-31 12:48:31 +0200

    enchanged code

--- src/intelligent_day_trading/trade_engine/core/experiment_management.py
+++ src/intelligent_day_trading/trade_engine/core/experiment_management.py
@@ -3,7 +3,6 @@ from typing import Any
 
 import yaml
 
-
 def to_yaml(data: dict) -> str:
     return yaml.safe_dump(
         data,
@@ -12,127 +11,156 @@ def to_yaml(data: dict) -> str:
         default_flow_style=False,
     )
 
+
 def filter_items(
+    root_element: str,
     items: list[dict[str, Any]],
-    match_path: str,
-    match_values: [str],
-) -> list[dict[str, Any]]:
-    """
-    Filter items using a dot-separated path.
+    filters: list[dict[str, Any]],
+) -> dict[str, Any]:
 
-    Example:
-
-        match_path  = "Profile.Name"
-        match_value = "asyafoek-stocks-long-swing-paper"
-    """
-
-    path_parts = match_path.split(".")
     matches = []
 
     for item in items:
-        current = item
 
-        for part in path_parts:
-            if not isinstance(current, dict):
-                current = None
+        matched = True
+
+        for filter_definition in filters:
+
+            current = item
+
+            path_parts = filter_definition["MatchPath"].split(".")
+            match_values = filter_definition["MatchValues"]
+
+            for part in path_parts:
+                if not isinstance(current, dict):
+                    current = None
+                    break
+
+                current = current.get(part)
+
+                if current is None:
+                    break
+
+            if current not in match_values:
+                matched = False
                 break
 
-            current = current.get(part)
-
-            if current is None:
-                break
-
-        if current in match_values:
+        if matched:
             matches.append(item)
 
-    return matches
-
+    return {
+        root_element: matches
+    }
 
 def retrieve_candidate_configuration(
     yaml_file_path: str,
     root_element: str,
-    match_path: str,
-    match_values: [str],
-) -> list[dict[str, Any]]:
-    """
-    Example:
+    filters: list[dict[str, Any]],
+) -> dict[str, list[dict[str, Any]]]:
 
-        retrieve_candidate_configuration(
-            yaml_file_path="profiles.yaml",
-            root_element="Profiles",
-            match_path="Profile.Name",
-            match_values=["asyafoek-stocks-long-swing-paper"],
-        )
-
-        retrieve_candidate_configuration(
-            yaml_file_path="riskreward.yaml",
-            root_element="RiskRewardNotations",
-            match_path="ProfileName",
-            match_values=[]"asyafoek-stocks-long-swing-paper"],
-        )
-    """
-
-    with Path(yaml_file_path).open("r", encoding="utf-8") as file:
+    with Path(yaml_file_path).open(
+        "r",
+        encoding="utf-8",
+    ) as file:
         configuration = yaml.safe_load(file)
 
     items = configuration.get(root_element, [])
 
-    return {
-        root_element: filter_items(
-            items=items,
-            match_path=match_path,
-            match_values=match_values,
-        )
-    }
-
+    return filter_items(
+        root_element=root_element,
+        items=items,
+        filters=filters,
+    )
 
 # Profiles example
 filename = "../../../../../corporate-it-intelligent-daytrading/src/resources/application-data/profiles.yaml"
 paper_profile = retrieve_candidate_configuration(
     yaml_file_path=filename,
     root_element="Profiles",
-    match_path="Profile.Name",
-    match_values=["asyafoek-stocks-long-swing-paper"],
+
+    filters=[
+        {
+            "MatchPath": "Profile.Name",
+            "MatchValues": [
+                "asyafoek-stocks-long-swing-paper",
+            ]
+        }
+    ]
 )
-print("Config Profiles\n", to_yaml(paper_profile))
+print(f"Config Profiles\n{to_yaml(paper_profile)}\n")
 
 # RiskReward example
 filename = "../../../../../corporate-it-intelligent-daytrading/src/resources/application-data/riskreward.yaml"
 paper_riskreward = retrieve_candidate_configuration(
     yaml_file_path=filename,
     root_element="RiskRewardNotations",
-    match_path="ProfileName",
-    match_values=["asyafoek-stocks-long-swing-paper"],
+    filters=[
+        {
+            "MatchPath": "ProfileName",
+            "MatchValues": [
+                "asyafoek-stocks-long-swing-paper",
+            ],
+        },
+        {
+            "MatchPath": "TrendRegime",
+            "MatchValues": [
+                "BEARISH",
+            ],
+        },
+        {
+            "MatchPath": "VolatilityRegime",
+            "MatchValues": [
+                "QUIET",
+            ],
+        }
+    ],
 )
-print("Config RiskReward\n", to_yaml(paper_riskreward))
+print(f"Config RiskReward\n{to_yaml(paper_riskreward)}\n")
+
 
 # RuleProviders example
 filename = "../../../../../corporate-it-intelligent-daytrading/src/resources/reference-data/rule_providers.yaml"
 paper_ruleproviders = retrieve_candidate_configuration(
     yaml_file_path=filename,
     root_element="RuleProviders",
-    match_path="EngineVersion",
-    match_values=[1],
+    filters=[
+        {
+            "MatchPath": "EngineVersion",
+            "MatchValues": [1],
+        }
+    ],
 )
-print("Config RuleProviders\n", to_yaml(paper_ruleproviders))
+print(f"Config RuleProviders\n{to_yaml(paper_ruleproviders)}\n")
 
 # Current Classification example
 filename = "../../../../../corporate-it-intelligent-daytrading/src/resources/application-data/tradeclassifications.yaml"
 current_classification = retrieve_candidate_configuration(
     yaml_file_path=filename,
     root_element="TradeClassifications",
-    match_path="Classification",
-    match_values=["GoodLoss"],
+    filters=[
+        {
+            "MatchPath": "Classification",
+            "MatchValues": [
+                "GoodLoss",
+            ],
+        }
+    ],
 )
-print("Current Classification\n", to_yaml(current_classification))
+print(f"Current Classification\n{to_yaml(current_classification)}\n")
 
 # Target Classification example
 filename = "../../../../../corporate-it-intelligent-daytrading/src/resources/application-data/tradeclassifications.yaml"
 target_classification = retrieve_candidate_configuration(
     yaml_file_path=filename,
     root_element="TradeClassifications",
-    match_path="Classification",
-    match_values=["GoodProfit"],
+    filters=[
+        {
+            "MatchPath": "Classification",
+            "MatchValues": [
+                "GoodProfit",
+            ],
+        }
+    ],
 )
-print("Target Classification\n", to_yaml(target_classification))
+print(f"Target Classification\n{to_yaml(target_classification)}\n")
 
